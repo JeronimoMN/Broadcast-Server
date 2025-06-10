21b2eb9569c8c98a0a78a5c1bfd82558e913ea41
jeroMuriNarva <jeronimomurillonarvaez@gmail.com>
2025-06-09 22:35:59 -0500

    Clean Code

--- src/Models/client.py
+++ src/Models/client.py
@@ -1,6 +1,4 @@
 import asyncio
-import json
-import time
 import websockets
 from prompt_toolkit import PromptSession
 from prompt_toolkit.patch_stdout import patch_stdout
@@ -13,7 +11,6 @@ class BroadcastClient:
         self.session = PromptSession()
 
     #TODO: CONNECT VALIDATOR: It have to consult if the server is active or not. If is not active, close wit no error and give context
-
 
     async def send_message(self, ws):
         while True:
@@ -31,7 +28,6 @@ class BroadcastClient:
                 print("Connection Closed", e)
                 break
 
-
     async def chat(self):
         try:
             async with websockets.connect(self.uri) as ws:
@@ -43,34 +39,6 @@ class BroadcastClient:
 
         except Exception as e:
             print("Error: ", e)
-        """
-        try:
-            async with websockets.connect(self.uri) as ws:
-                while True:
-                    #Problem: The flow of the code is bloked by the msg Input. It has to be Asyncronymus
-                    msg = input("Enter Message: ")
-                    message = {
-                        "msg": msg,
-                        "info": {
-                            "type": "message",
-                            "author": self.name,
-                            "time": time.time()
-                        }
-                    }
-                    if msg is not None:
-                        await ws.send(msg)
-                    print(f"Received: {await ws.recv()}")
-        except websockets.exceptions.ConnectionClosedError as e:
-            print(f"Connection Closed: {e}")
-
-    async def disconnect_server(self):
-        try:
-            await self.ws.close(1000, 'Good')
-            print('Disconnected from server')
-        except Exception as e:
-            print(e)
-    """
-
 
 if __name__ == '__main__':
     client = BroadcastClient('clientPP')
